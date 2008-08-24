d4af3600c3b76d8b6e91d71e51bfb29261c2df76
Peter Karman <perl@peknet.com>
2008-08-24 03:53:32 +0000

    Import of KARMAN/CatalystX-CRUD-Model-RDBO-0.13 from CPAN.
    gitpan-cpan-distribution: CatalystX-CRUD-Model-RDBO
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         KARMAN/CatalystX-CRUD-Model-RDBO-0.13.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- t/lib/My/Bar.pm
+++ t/lib/My/Bar.pm
@@ -1,16 +1,16 @@
-package My::Foo;
+package My::Bar;
 use strict;
-use base qw(
-    Rose::DB::Object
-    Rose::DB::Object::Helpers
-    Rose::DBx::Object::MoreHelpers
+use base qw( 
+    Rose::DB::Object 
+    Rose::DB::Object::Helpers 
+    Rose::DBx::Object::MoreHelpers    
 );
 use Carp;
 use Data::Dump qw( dump );
 use My::DB;
 
 __PACKAGE__->meta->setup(
-    table   => 'foos',
+    table   => 'bars',
     columns => [
         id   => { type => 'serial',  not_null => 1, primary_key => 1 },
         name => { type => 'varchar', length   => 16 },
@@ -19,13 +19,13 @@ __PACKAGE__->meta->setup(
     primary_key_columns => ['id'],
 
     relationships => [
-        bar => {
+        foo => {
             class      => 'My::FooBar',
-            column_map => { id => 'foo_id' },
+            column_map => { id => 'bar_id' },
             type       => 'one to many',
         },
 
-        bars => {
+        foos => {
             map_class => 'My::FooBar',
             type      => 'many to many',
         }
